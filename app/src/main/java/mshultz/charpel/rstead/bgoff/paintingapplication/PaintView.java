922496e91da503fdb5676b969489a9240511663c
MShultz <mshultz@student.neumont.edu>
2017-04-19 11:24:38 -0600

    Implementation of constructor

--- app/src/main/java/mshultz/charpel/rstead/bgoff/paintingapplication/PaintView.java
+++ app/src/main/java/mshultz/charpel/rstead/bgoff/paintingapplication/PaintView.java
@@ -2,6 +2,9 @@ package mshultz.charpel.rstead.bgoff.paintingapplication;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -13,10 +16,28 @@ import android.view.View;
 
 public class PaintView extends View {
 
+    private Context context;
+    private Paint painter;
+    private Path path;
+
     public PaintView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        this.setContext(context);
+        path = new Path();
+
+        painter = new Paint();
+        painter.setAntiAlias(true);
+        painter.setColor(Color.BLACK);
+        painter.setStrokeJoin(Paint.Join.ROUND);
+        painter.setStyle(Paint.Style.STROKE);
+        painter.setStrokeWidth(5f);
     }
 
+
+    public void clearCanvas(){
+
+    }
     @Override
     protected void onDraw(Canvas canvas){
 
@@ -26,5 +47,9 @@ public class PaintView extends View {
     public boolean onTouchEvent(MotionEvent event){
 
         return false;
+    }
+
+    private void setContext(Context context){
+        this.context = context;
     }
 }
