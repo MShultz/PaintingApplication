e330b31b142c3d8fcd3e6c3979689ddf64fcd5fe
timafer <boomer3000@comcast.net>
2017-04-20 11:47:17 -0600

    added back end for color sliders

--- app/src/main/java/mshultz/charpel/rstead/bgoff/paintingapplication/MainActivity.java
+++ app/src/main/java/mshultz/charpel/rstead/bgoff/paintingapplication/MainActivity.java
@@ -2,12 +2,16 @@ package mshultz.charpel.rstead.bgoff.paintingapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private PaintView paintView;
-
+    private SeekBar redSlider,greenSlider,blueSlider;
+    private int redValue=255,greenValue=255,blueValue=255;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +19,47 @@ public class MainActivity extends AppCompatActivity {
 
         paintView = (PaintView)findViewById(R.id.signature_canvas);
 
+       // setRGBSlider(redSlider,R.id.Red);
+        //setRGBSlider(greenSlider,R.id.Green);
+        //setRGBSlider(blueSlider,R.id.Blue);
     }
+    /*private void setRGBSlider(SeekBar seekBar,int Id){
+        seekBar=(SeekBar)findViewById(Id);
+        seekBar.setMax(255);
+        seekBar.setProgress(255);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateColor(seekBar,progress);
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    public void updateColor(SeekBar seekBar,int progress){
+        switch (seekBar.getId()){
+            case R.id.Red:
+                redValue=progress;
+                break;
+            case R.id.Green:
+                greenValue=progress;
+                break;
+            case R.id.Blue:
+                blueValue=progress;
+                break;
+            default:
+                Log.e("Invalid Slider","Update Color");
+        }
+        paintView.setColor(255,redValue,greenValue,blueValue);
+    }*/
     public void clearCanvas(View view){
         paintView.clearCanvas();
     }
