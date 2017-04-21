11c505ce13f53c3d4d772ab951fe5b517a37686d
rzstead <kingstead1@gmail.com>
2017-04-20 20:40:39 -0600

    Fixed merge issues

--- app/src/main/java/mshultz/charpel/rstead/bgoff/paintingapplication/MainActivity.java
+++ app/src/main/java/mshultz/charpel/rstead/bgoff/paintingapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package mshultz.charpel.rstead.bgoff.paintingapplication;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;mport android.app.DialogFragment;
-import android.content.res.Configuration;import android.support.design.widget.BottomSheetBehavior;
+import android.graphics.BitmapFactory;
+import android.app.DialogFragment;
+import android.content.res.Configuration;
+import android.support.design.widget.BottomSheetBehavior;
+//import android.support.v4.app.DialogFragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,26 +15,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Button;import android.widget.SeekBar;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity implements SliderDialogue.SliderDialogueListener{    DialogFragment dialogue;    private PaintView paintView;
+public class MainActivity extends AppCompatActivity implements SliderDialogue.SliderDialogueListener {
+    DialogFragment dialogue;
+    private PaintView paintView;
     private SeekBar redSlider, greenSlider, blueSlider;
     private int redValue = 255, greenValue = 255, blueValue = 255;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         paintView = (PaintView) findViewById(R.id.signature_canvas);
     }
+
     public void clearCanvas(View view) {
         paintView.clearCanvas();
     }
-    public void setEraser(View view){paintView.setEraser();}
 
-    public void setBrushShape(View view){
+    public void setEraser(View view) {
+        paintView.setEraser();
+    }
+
+    public void setBrushShape(View view) {
         Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.lips);
         paintView.setBrushImage(image);
     }
@@ -39,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements SliderDialogue.Sl
     public void setColor(View view) {
         dialogue = new SliderDialogue();
         dialogue.show(getFragmentManager(), "Hello");
-}    }
+    }
 
 
     public void setBrushSize(View view) {
@@ -48,9 +59,10 @@ public class MainActivity extends AppCompatActivity implements SliderDialogue.Sl
 
     @Override
     public void onOKClick(DialogFragment dialog) {
-        redValue = ((SliderDialogue)dialogue).getRedValue();
-        blueValue = ((SliderDialogue)dialogue).getBlueValue();
-        greenValue = ((SliderDialogue)dialogue).getGreenValue();
+        redValue = ((SliderDialogue) dialogue).getRedValue();
+        blueValue = ((SliderDialogue) dialogue).getBlueValue();
+        greenValue = ((SliderDialogue) dialogue).getGreenValue();
         paintView.setColor(255, redValue, greenValue, blueValue);
     }
+
 }
